84cecbdf4a4a7d5688e3194993981f45ca7f9537
Yann Boutin <yboutin@enovacom.fr>
2019-11-08 12:19:06 +0100

    Publication de js2json 133efe35e6387542b29af86ed87ab3ad99bcef5b

---
+++ js2json/precache-manifest.c18e7d2988b684ca1c0927e7da90eb90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7cb41b7009f08bfef213633ca65a305",
+    "url": "/js2json/index.html"
+  },
+  {
+    "revision": "977a88df75f232d3640f",
+    "url": "/js2json/static/css/main.8365d79c.chunk.css"
+  },
+  {
+    "revision": "021caf25cdd45aa9876f",
+    "url": "/js2json/static/js/2.94226417.chunk.js"
+  },
+  {
+    "revision": "977a88df75f232d3640f",
+    "url": "/js2json/static/js/main.8aaa6ca5.chunk.js"
+  },
+  {
+    "revision": "7971ce063e5d6fc624ac",
+    "url": "/js2json/static/js/runtime-main.7c348ed5.js"
+  }
+]);
