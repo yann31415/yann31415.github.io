fd6a61176a99ef5ae1f8f86c0a8f3d4c289a1d4f
Yann Boutin <yboutin@enovacom.fr>
2020-01-29 15:42:23 +0100

    ---

---
+++ js2json/precache-manifest.0caa585f7d986428abd6cb38ff7fbbf0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e5fcd2a197d0fbcdb8096d5a5f7c77",
+    "url": "/js2json/index.html"
+  },
+  {
+    "revision": "f510a2876892cee5543b",
+    "url": "/js2json/static/css/main.8365d79c.chunk.css"
+  },
+  {
+    "revision": "021caf25cdd45aa9876f",
+    "url": "/js2json/static/js/2.94226417.chunk.js"
+  },
+  {
+    "revision": "f510a2876892cee5543b",
+    "url": "/js2json/static/js/main.b100719e.chunk.js"
+  },
+  {
+    "revision": "7971ce063e5d6fc624ac",
+    "url": "/js2json/static/js/runtime-main.7c348ed5.js"
+  }
+]);
